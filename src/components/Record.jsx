934b4e738b0d4dbaa8b0d4f4fb7f660f560145c0
alexholmes99 <alexh@chillibean.com>
2023-08-12 16:59:20 +0100

    chore: basic music playing (w/ error)

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
+import Sound from "react-sound";
+import SutureFuture from "../assets/music/suturefuture.mp3";
 
 function Record() {
-  return <Img src={require("../assets/images/record.png")} alt="Record" />;
+  const [sound, setSound] = useState(Sound.status.STOPPED);
+
+  return (
+    <RecordContainer>
+      <Sound url={SutureFuture} playStatus={sound} />
+      <Img
+        onMouseEnter={() => setSound(Sound.status.PLAYING)}
+        onMouseLeave={() => setSound(Sound.status.PAUSED)}
+        src={require("../assets/images/record.png")}
+        alt="Record"
+      />
+    </RecordContainer>
+  );
 }
+
+const RecordContainer = styled.div`
+  width: 50%;
+  height: 50%;
+`;
 
 const Img = styled.img`
   &:hover {
-    animation: rotation 2s infinite linear;
+    animation: rotation 1.5s infinite linear;
   }
 
   @keyframes rotation {
